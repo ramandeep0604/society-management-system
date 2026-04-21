3dcf5328af97783b16075cc78c848b7751c96a40
ramandeep0604 <ramandeepkaur4619@gmail.com>
2026-04-21 22:03:39 +0530

    feat: added vercel.json

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app= express();
+
+
+app.get('/health',(req,res)=>{
+res.send("health is ok")
+})
 app.listen(3000,()=>{
 console.log("server is running");
 
